71b89fb08f8f93490d29bfd798b38db874b1545d
Alan Vardy <alanvardy@gmail.com>
2018-12-18 13:27:12 -0700

    add checks and app.json

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'main/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
+  get '/check.txt', to: proc {[200, {}, ['it_works']]}
 end
