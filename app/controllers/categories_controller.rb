e3af2d758b510a2f1b527622da09b02be88e3837
Alan Vardy <alanvardy@gmail.com>
2018-12-19 12:39:35 -0700

    require authentication and fix login logout

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :authentication_required!
 
   # GET /categories
   # GET /categories.json
