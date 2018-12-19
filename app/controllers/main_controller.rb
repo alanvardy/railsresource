79d34556e6c102a3cd54099b74a50fe72c969db8
Alan Vardy <alanvardy@gmail.com>
2018-12-19 15:47:09 -0700

    add search functionality

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def index
-    @categories = Category.all
+    @category = Category.get(params[:id])
+    @title = @category.title
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,17 @@
 class Category < ApplicationRecord
   has_many :topics
   has_many :resources, through: :topics
+
+  def self.get(id)
+    case id
+    when "models"
+      find_by(title: 'Models')
+    when "views"
+      find_by(title: 'Views')
+    when "controllers"
+      find_by(title: 'Controllers')
+    else
+      find_by(title: 'Models')
+    end
+  end
 end
