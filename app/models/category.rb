7b56c028f07144d859e8b53076ca2453f3514b1e
Alan Vardy <alanvardy@gmail.com>
2018-12-27 15:34:52 -0700

    modified log messaging

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,14 +4,14 @@ class Category < ApplicationRecord
 
   def self.get(id)
     if id.nil?
-      return Rails.cache.fetch('models') { puts 'not cached'; find_by(title: 'Models') }
+      return Rails.cache.fetch('models') { Rails.logger.info "NOT CACHED"; find_by(title: 'Models') }
     else
       id.capitalize!
     end
 
     file_not_found if Category.all.none? { |cat| cat.title == id }
 
-    Rails.cache.fetch(id) { puts 'not cached'; find_by(title: id) }
+    Rails.cache.fetch(id) { Rails.logger.info "NOT CACHED"; find_by(title: id) }
   end
 
   def file_not_found
